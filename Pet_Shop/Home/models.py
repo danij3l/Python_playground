1d3d6cdb6412b80d168c9f20dd09cefd85f44c4d
Danijel <danijelpavic21@gmail.com>
2022-11-24 22:22:25 +0100

    Created class Animals.

--- Pet_Shop/Home/models.py
+++ Pet_Shop/Home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+""" Svaka životinja ima ID, ime,
+kategoriju, URLove do slika, tagove i statuse. 
+Status može biti placed/approved/delivered.
+Potrebno je omogućiti dodavanje, uređivanje, brisanje i 
+pregledavanje životinja, kategorija,
+tagova. """
+
+class Animals(models.Model):
+
+    STATUS_CHOICES = (
+        (PLACED, u'Placed'),
+        (APPROVED, u'Approved'),
+        (DELIVERED, u'Delivered'),
+    )
+
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=200)
+    picture_url = models.CharField()
+    tags = models.CharField()
+    status = models.CharField(
+    u'Status', max_length=20,
+    choices=STATUS_CHOICES)
