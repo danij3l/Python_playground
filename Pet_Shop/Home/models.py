b9ff1473b0c5609eacf81fd5f8e80b379e0eb9d4
Danijel <danijelpavic21@gmail.com>
2022-11-25 14:26:47 +0100

    Tested and added two new animals in admin - the save saves images in static/pictures folder.

--- Pet_Shop/Home/models.py
+++ Pet_Shop/Home/models.py
@@ -10,9 +10,9 @@ tagova. """
 class Animals(models.Model):
 
     STATUS_CHOICES = (
-        (PLACED, u'Placed'),
-        (APPROVED, u'Approved'),
-        (DELIVERED, u'Delivered'),
+        ("PLACED", u'Placed'),
+        ("APPROVED", u'Approved'),
+        ("DELIVERED", u'Delivered'),
     )
 
 
@@ -20,8 +20,18 @@ class Animals(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=200)
     picture = models.ImageField(null=True, blank=False)
-    tags = models.CharField()
+    tags = models.CharField(max_length=200)
     status = models.CharField(
     u'Status', max_length=20,
     choices=STATUS_CHOICES)
-    #edit this
+    
+    def __str__(self):
+        return self.name
+
+    @property
+    def pictureURL(self):
+        try:
+            url = self.pictureURL
+        except:
+            url= ''
+        return url
