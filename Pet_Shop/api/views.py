dc7dccf7874b2898c912be7fbc7dca1a29197649
Danijel <danijelpavic21@gmail.com>
2022-11-27 17:25:33 +0100

    Created post metod. There's no error on client except object is not stored in db - found out the error - "The submitted data was not a file".

--- Pet_Shop/api/views.py
+++ Pet_Shop/api/views.py
@@ -8,3 +8,12 @@ def getData(request):
     animals = Animals.objects.all()
     serializer = AnimalSerializer(animals, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def addAnimal(request):
+    serializer = AnimalSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        print(serializer.errors)
+        # [ErrorDetail(string='The submitted data was not a file. Check the encoding type on the form.', code='invalid')]
+    return Response(serializer.data) 
