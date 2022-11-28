792a3bf718aadb616e4ff2632eaa0f9555e98da5
Danijel <danijelpavic21@gmail.com>
2022-11-28 20:15:08 +0100

    Completed CRUD with adding update or delete option to api with generic class based view.

--- Pet_Shop/api/urls.py
+++ Pet_Shop/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getData),
     path('add/', views.addAnimal),
+    path('updateOrDelete/<int:pk>', views.AnimalDetail.as_view()),
 ]
--- Pet_Shop/api/views.py
+++ Pet_Shop/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from Home.models import Animals
 from .serializers import AnimalSerializer
@@ -17,3 +18,8 @@ def addAnimal(request):
         print(serializer.errors)
         # [ErrorDetail(string='The submitted data was not a file. Check the encoding type on the form.', code='invalid')]
     return Response(serializer.data) 
+
+#class based view -> we fetch object by Id in url and can update or delete it
+class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class=AnimalSerializer
+    queryset = Animals.objects.all()
